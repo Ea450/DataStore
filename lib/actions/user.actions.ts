e65f42bfde6841c3b3df854575c4e869e2fa58e8
Eslam <ea450424@gmail.com>
2025-08-28 05:00:13 +0300

    add OTP

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import { createAdminClient } from "../appwrite";
 import { appwriteConfig } from "../appwrite/config";
 import { avatarPlaceholderUrl } from "@/constants";
 import { parseStringify } from "../utils";
+import { cookies } from "next/headers";
 const getUserByEmail = async (email: string) => {
   const { databases } = await createAdminClient();
 
@@ -19,7 +20,7 @@ const handleError = (error: unknown, message: string) => {
   console.log(error, message);
   throw error;
 };
-const sendEmailOTP = async ({ email }: { email: string }) => {
+export const sendEmailOTP = async ({ email }: { email: string }) => {
   const { account } = await createAdminClient();
   try {
     const session = await account.createEmailToken(ID.unique(), email);
@@ -55,4 +56,25 @@ export const createAccount = async ({
     );
   }
   return parseStringify({ accountId });
+};
+export const verifySecret = async ({
+  accountId,
+  password,
+}: {
+  accountId: string;
+  password: string;
+}) => {
+  try {
+    const { account } = await createAdminClient();
+    const session = await account.createSession(accountId, password);
+    (await cookies()).set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+    return parseStringify({ sessionId: session.$id });
+  } catch (error) {
+    console.log(error);
+  }
 };
